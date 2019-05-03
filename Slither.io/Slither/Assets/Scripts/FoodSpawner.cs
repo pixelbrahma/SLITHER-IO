b64e30c956d9ddedaf590908a21f4f08b1e66c13
Nikhil Bharadwaj <44049892+pixelbrahma@users.noreply.github.com>
2019-05-03 14:38:19 +0530

    adds

---
+++ Slither.io/Slither/Assets/Scripts/FoodSpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodSpawner : MonoBehaviour {
+
+    private float height;
+    private float width;
+    private Vector2 foodPosition;
+    [SerializeField] private float spawnSpeedCheck;
+    private int count = 0;
+    [SerializeField] Transform foodPrefab;
+
+    void Start() {
+        height = Camera.main.orthographicSize;
+        width = height * Camera.main.aspect;
+    }
+
+    void Update()
+    {
+        count++;
+        if (count >= spawnSpeedCheck)
+        {
+            count = 0;
+            foodPosition = new Vector2(Random.Range(width, -width), Random.Range(height, -height));
+            Transform food = Instantiate(foodPrefab, foodPosition, Quaternion.identity);
+        }
+    }
+}
---
+++ Slither.io/Slither/Assets/Scripts/SnakeController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class SnakeController : MonoBehaviour {
+
+    [SerializeField] private Transform tailPrefab;
+    private Rigidbody2D rb;
+    [SerializeField] private float moveSpeed;
+    private List<Transform> tails;
+    private bool ate = false;
+
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        InvokeRepeating("Move", 0.3f, 0.1f);
+        tails = new List<Transform>();
+	}
+
+	void Update ()
+    {
+       
+	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        ate = true;
+        Destroy(other.gameObject);
+    }
+
+    private void Move()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 direction = mousePos - transform.position;
+        direction = direction.normalized;
+        Vector2 headPosition = transform.position;
+        rb.velocity = direction * moveSpeed;
+        if(ate)
+        {
+            Transform tail = Instantiate(tailPrefab, headPosition, Quaternion.identity);
+            tails.Insert(0, tail);
+            ate = false;
+        }
+        else if(tails.Count>0)
+        {
+            tails.Last().position = headPosition;
+            tails.Insert(0, tails.Last());
+            tails.RemoveAt(tails.Count - 1);
+        }
+    }
+}
